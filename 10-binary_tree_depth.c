c1aa77f855f59a7d1514eba03d362b8c561a24e2
Antgst <ant.gousset@gmail.com>
2025-12-08 21:22:56 +0100

    add: add T10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Measure the depth of a node in a binary tree
+ * @tree: Pointer to the node to measure
+ * 
+ * Return: Depth of the node, or 0 if the tree is NULL
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+
+	while (tree->parent != NULL)
+		{
+			depth++;
+			tree = tree->parent;
+		}
+	
+		return (depth);
+
+}
