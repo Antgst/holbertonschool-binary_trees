e5208430897fec33c3daf8259df8aee5929db5d0
Antgst <ant.gousset@gmail.com>
2025-12-08 21:04:21 +0100

    fix: edit syntax of T5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,16 +4,16 @@
  * binary_tree_is_root - checks if a given node is root
  * @node: pointer to the node to check
  *
- * Return: 1 if the node is root, otherwise,
+ * Return: 1 if the node is root, 0 otherwise.
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
-	return (0);
+		return (0);
 
 	if (node->parent == NULL)
-	return (1);
+		return (1);
 
 	return (0);
 }
