081c6a803009a349b408c0b23de7721de4209ad5
Antgst <ant.gousset@gmail.com>
2025-12-08 20:41:12 +0100

    fix: edit syntax of T8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -4,15 +4,16 @@
  * binary_tree_postorder - traverse a binary tree using post-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
+ * 
+ * Return: Nothing
  */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
-	return;
+		return;
 
-	func(tree->n);
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
-
+	func(tree->n);
 }
