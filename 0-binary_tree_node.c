c06b1e95f58cf4ef3a46b483d84665f584cf26b9
Antgst <ant.gousset@gmail.com>
2025-12-08 18:59:18 +0100

    fix: fix the syntax of T0 and add stdlib

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,26 +1,26 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
-* binary_tree_node - Create a binary tree node
-* @parent: pointer to the parent node of the node to create
-* @value: value to put inside the new node
-*
-* Return: A pointer to the new node or NULL if it failed
-*/
+ * binary_tree_node - Create a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put inside the new node
+ *
+ * Return: A pointer to the new node or NULL if it failed
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-
-binary_tree_t *node;
-
-node = malloc(sizeof(binary_tree_t));
-if (node == NULL)
-	return (NULL);
-
-node->n = value;
-node->left = NULL;
-node->right = NULL;
-node->parent = parent;
-
-return (node);
+	binary_tree_t *node;
+	
+	node = malloc(sizeof(*node)); //ou ((binary_tree_t)) ?
+	if (node == NULL)
+		return (NULL);
+	
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = parent;
+	
+	return (node);
 }
