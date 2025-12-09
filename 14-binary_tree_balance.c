4a53e82e0843b397498689fd0c9fe29ea1c6148d
Antgst <ant.gousset@gmail.com>
2025-12-09 10:44:12 +0100

    add: T14 of the project, struggling with it so I pass to the 15

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Measure the balance
+ * @tree: Pointer to the root of the tree
+ * 
+ * Return: The balance factor, or 0 if the tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+
+}
