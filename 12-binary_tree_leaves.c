f01158324b514e89d3a4275a903b2d530ac39ae6
Antgst <ant.gousset@gmail.com>
2025-12-10 12:41:41 +0100

    fix: edit T12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,17 +3,17 @@
 /**
  * binary_tree_leaves - Count the leaves in a binary tree
  * @tree: Pointer to the root of the tree
- * 
+ *
  * Return: The number of leaves, or 0 if the tree is NULL
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
-    
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
