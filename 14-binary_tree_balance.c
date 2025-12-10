451ddee0a1ecd178a4a0bd9493cf946a55ad4d37
Antgst <ant.gousset@gmail.com>
2025-12-10 12:54:47 +0100

    fix: betty of 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,7 +30,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree
  *
- * Return: balance factor (left height - right height), or 0 if tree is NULL
+ * Return: The balance of the binary tree, or 0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
