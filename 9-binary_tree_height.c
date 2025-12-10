5d07fe9ae668e8792fe393ff63ad07e3cd738289
Gigi-Corlay <11489@holbertonstudents.com>
2025-12-10 12:25:47 +0100

    Correction to comply with Holberton's definition of height.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,14 +1,10 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree
  *
- * Description: Returns the height of the tree. The height is
- * defined as the number of nodes along the longest path from
- * the root node down to the farthest leaf node. Returns 0 if tree is NULL.
+ * Return: height of the tree, or 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -17,11 +13,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	/* Si c'est une feuille, height = 0 */
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height + 1);
-	else
-		return (right_height + 1);
+
+	return (right_height + 1);
 }
